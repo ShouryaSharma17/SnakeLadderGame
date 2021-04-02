77eedc06997aeb0a30a8a68bd28cfb878c8f1283
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-02 20:50:59 +0530

    get number between 1 to 6

--- SnakeLadderUC.java
+++ SnakeLadderUC.java
@@ -1,6 +1,11 @@
+import java.util.*;
 public class SnakeLadderUC {      
-	public static void main(String[] args) {                  
-      		int position=0;                                 
+	public static void main(String[] args) {   
+		Random random=new Random();
+		int diceRoll=random.nextInt(6);
+		int finalDiceRoll=diceRoll+1;               
+      		int position=0;              
+		System.out.println("Dice roll no. is" +finalDiceRoll);                   
                 System.out.println("Position " +position);      
 		}
 }
