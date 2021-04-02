4a8e7a1a16f1c4c98590bf8a31aae156a46d8506
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-02 20:37:28 +0530

    Position set to zero

---
+++ SnakeLadderUC.java
@@ -0,0 +1,6 @@
+public class SnakeLadderUC {      
+	public static void main(String[] args) {                  
+      		int position=0;                                 
+                System.out.println("Position " +position);      
+		}
+}
