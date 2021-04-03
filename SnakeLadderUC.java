683b6dda9924f1882125491c8c2c316d025f4814
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-03 10:11:33 +0530

    Number of times the dice was played to win the game

--- SnakeLadderUC.java
+++ SnakeLadderUC.java
@@ -1,31 +1,44 @@
-import java.util.*;
+import java.util.Random;
+
 public class SnakeLadderUC {
-	public static void main(String[] args) {
-		Random random= new Random();
-		//CONSTANTS
-		int NO_PLAY = 0;
-		int LADDER = 1;
-		int SNAKE = 2;
-		int counter = 0;
-		//VARIABLE
-      		int position= 0;
-		while (position <= 100) {
-			counter++;
-			int diceRoll = random.nextInt(6);
-			int options = random.nextInt(3);
-			int finalDiceRoll = diceRoll + 1;
-		System.out.println("dice roll no. is" + finalDiceRoll);
-		System.out.println("playing option is" + options);
-		if (options == NO_PLAY)
-			position = position + 0;
-		else if (options == LADDER)
-			position = position + finalDiceRoll;
-		else {
-			position = position - finalDiceRoll;
-			if (position <= 0)
-				position = 0;
-		     }
-		     System.out.println("Current position:" +position);
-		}
-		}
+    public static void main(String args[]) {
+        Random random = new Random();
+
+
+        //Constants
+        int NO_PLAY = 0;
+        int LADDER = 1;
+        int SNAKE = 2;
+        int counter = 0;
+
+        //Variables
+        int position = 0;
+
+        while (position < 100) {
+            counter++;
+
+            int diceRoll = random.nextInt(6);
+            int options = random.nextInt(3);
+            int finalDiceRoll = diceRoll + 1;
+            System.out.println("dice roll no is: " + finalDiceRoll);
+            System.out.println("playing option is :" + options);
+            if (options == NO_PLAY)
+                position = position + 0;
+            else if (options == LADDER)
+                if (position + finalDiceRoll > 100)
+                    position = position + 0;
+                else
+                    position = position + finalDiceRoll;
+
+            else {
+                position = position - finalDiceRoll;
+                if (position <= 0)
+                    position = 0;
+
+
+
+            }
+            System.out.println("dice roll no:" + counter + " current position:" + position);
+        }
+    }
 }
